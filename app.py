54c8b27b2937b8487301e815716fadb3220a0f73
Brian <brian2000bett@gmail.com>
2025-06-25 12:12:24 +0300

    tables created

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ CORS(app)
 
 #configuring our app
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 #inorder to see our sql statements being logged out; 
 app.config["SQLALCHEMY_ECHO"] = True
@@ -23,3 +25,4 @@ app.config["SQLALCHEMY_ECHO"] = True
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
